68d7bc4f84593e15bb197d4e160d413955f71080
alvaro <alvarorga@hotmail.com>
2018-05-16 11:54:34 +0200

    TST: make sparse tests not depend on random matrices

--- tests/test_sparse_operators.py
+++ tests/test_sparse_operators.py
@@ -11,79 +11,58 @@ from pyexact import sparse_operators
 class SparseManyBodyOperatorsTestCase(unittest.TestCase):
     """Test for the functions that build sparse many body operators."""
 
-    def test_sparse_coincides_with_dense(self):
-        """Test that sparse Hamiltonians coincide with dense ones."""
-        L = 12
-        N = 6
-        J = np.random.rand(L, L)
-        D = np.random.rand(L, L)
-        dense = dense_operators.de_op(L, N, J, D)
-        v, r, c, ns = sparse_operators.sp_op(L, N, J, D)
-        H = csr_matrix((v, (r, c)), shape=(ns, ns)).toarray()
-        self.assertTrue(np.allclose(dense, H))
-
-    def test_symmetric_sparse_coincides_with_dense(self):
-        """Test that sparse Hamiltonians coincide with dense ones."""
-        L = 12
-        N = 6
-        J = np.random.rand(L, L)
-        J = (J + J.T)/2
-        D = np.random.rand(L, L)
-        D = (D + D.T)/2
-        dense = dense_operators.de_op(L, N, J, D)
-        v, r, c, ns = sparse_operators.sp_op(L, N, J, D)
-        H = csr_matrix((v, (r, c)), shape=(ns, ns)).toarray()
-        self.assertTrue(np.allclose(dense, H))
-
     def test_values_in_operator_matrix(self):
-        """Test several values in a general random operator."""
-        J = 1 - 2*np.random.rand(5, 5)
-        D = 1 - 2*np.random.rand(5, 5)
-        Op = dense_operators.de_op(5, 3, J, D)
-        # Temporary values to compare.
-        t_val_0_0 = (J[0, 0]+J[1, 1]+J[2, 2]
-                    +D[0, 1]+D[0, 2]+D[1, 2]
-                    +D[1, 0]+D[2, 0]+D[2, 1])
-        self.assertAlmostEqual(Op[0, 0], t_val_0_0)
-        t_val_3_3 = (J[1, 1]+J[2, 2]+J[3, 3]
-                     +D[1, 2]+D[1, 3]+D[2, 3]
-                     +D[2, 1]+D[3, 1]+D[3, 2])
-        self.assertAlmostEqual(Op[3, 3], t_val_3_3)
-        t_val_6_6 = (J[1, 1]+J[2, 2]+J[4, 4]
-                     +D[1, 2]+D[1, 4]+D[2, 4]
-                     +D[2, 1]+D[4, 1]+D[4, 2])
-        self.assertAlmostEqual(Op[6, 6], t_val_6_6)
-        self.assertAlmostEqual(Op[4, 7], J[1, 3])
-        self.assertAlmostEqual(Op[4, 9], 0)
-        self.assertAlmostEqual(Op[0, 9], 0)
-        self.assertAlmostEqual(Op[5, 4], J[2, 1])
-        self.assertAlmostEqual(Op[5, 7], J[2, 3])
+        """Test several values of a general operator."""
+        J = np.reshape(np.arange(25), (5, 5))
+        D = np.reshape(25+np.arange(25), (5, 5))
+        v, r, c, ns = sparse_operators.sp_pc_op(5, 3, J, D)
+        H = csr_matrix((v, (r, c)), shape=(ns, ns)).toarray()
+        self.assertAlmostEqual(H[0, 0], 204)
+        self.assertAlmostEqual(H[1, 1], 222)
+        self.assertAlmostEqual(H[3, 3], 258)
+        self.assertAlmostEqual(H[5, 5], 258)
+        self.assertAlmostEqual(H[9, 9], 312)
+        self.assertAlmostEqual(H[0, 1], 13)
+        self.assertAlmostEqual(H[0, 3], 3)
+        self.assertAlmostEqual(H[0, 5], 9)
+        self.assertAlmostEqual(H[0, 9], 0)
+        self.assertAlmostEqual(H[3, 5], 0)
+        self.assertAlmostEqual(H[4, 5], 7)
+        self.assertAlmostEqual(H[6, 9], 8)
+        self.assertAlmostEqual(H[7, 5], 17)
+        self.assertAlmostEqual(H[7, 6], 0)
+        self.assertAlmostEqual(H[8, 2], 0)
+        self.assertAlmostEqual(H[8, 3], 22)
+        self.assertAlmostEqual(H[9, 2], 20)
 
     def test_values_in_symmetric_operator_matrix(self):
-        """Test several values in a general random operator."""
-        J = 1 - 2*np.random.rand(5, 5)
-        J = (J + J.T)/2
-        D = 1 - 2*np.random.rand(5, 5)
-        D = (D + D.T)/2
-        Op = dense_operators.de_op(5, 3, J, D)
-        # Temporary values to compare.
-        t_val_0_0 = (J[0, 0]+J[1, 1]+J[2, 2]
-                    +D[0, 1]+D[0, 2]+D[1, 2]
-                    +D[1, 0]+D[2, 0]+D[2, 1])
-        self.assertAlmostEqual(Op[0, 0], t_val_0_0)
-        t_val_3_3 = (J[1, 1]+J[2, 2]+J[3, 3]
-                     +D[1, 2]+D[1, 3]+D[2, 3]
-                     +D[2, 1]+D[3, 1]+D[3, 2])
-        self.assertAlmostEqual(Op[3, 3], t_val_3_3)
-        t_val_6_6 = (J[1, 1]+J[2, 2]+J[4, 4]
-                     +D[1, 2]+D[1, 4]+D[2, 4]
-                     +D[2, 1]+D[4, 1]+D[4, 2])
-        self.assertAlmostEqual(Op[6, 6], t_val_6_6)
-        self.assertAlmostEqual(Op[4, 7], J[1, 3])
-        self.assertAlmostEqual(Op[4, 9], 0)
-        self.assertAlmostEqual(Op[0, 9], 0)
-        self.assertAlmostEqual(Op[5, 4], J[2, 1])
-        self.assertAlmostEqual(Op[5, 7], J[2, 3])
+        """Test several values of a general symmetric operator."""
+        J = np.reshape(np.arange(25), (5, 5))
+        D = np.reshape(25+np.arange(25), (5, 5))
+        # Make J and D symmetric.
+        for j in range(5):
+            for i in range(j):
+                J[j, i] = J[i, j]
+                D[j, i] = D[i, j]
+        v, r, c, ns = sparse_operators.sp_sym_pc_op(5, 3, J, D)
+        H = csr_matrix((v, (r, c)), shape=(ns, ns)).toarray()
+        self.assertAlmostEqual(H[0, 0], 188)
+        self.assertAlmostEqual(H[1, 1], 198)
+        self.assertAlmostEqual(H[3, 3], 242)
+        self.assertAlmostEqual(H[5, 5], 226)
+        self.assertAlmostEqual(H[9, 9], 296)
+        self.assertAlmostEqual(H[0, 1], 13)
+        self.assertAlmostEqual(H[0, 3], 3)
+        self.assertAlmostEqual(H[0, 5], 9)
+        self.assertAlmostEqual(H[0, 9], 0)
+        self.assertAlmostEqual(H[3, 5], 0)
+        self.assertAlmostEqual(H[4, 5], 7)
+        self.assertAlmostEqual(H[6, 9], 8)
+        self.assertAlmostEqual(H[7, 5], 13)
+        self.assertAlmostEqual(H[7, 6], 0)
+        self.assertAlmostEqual(H[8, 2], 0)
+        self.assertAlmostEqual(H[8, 3], 14)
+        self.assertAlmostEqual(H[9, 2], 4)
 
 if __name__ == '__main__':
     unittest.main()
