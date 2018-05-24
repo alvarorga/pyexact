f46db61eb8be17b4de0d8ca60f89ef5ac34acfc1
alvaro <alvarorga@hotmail.com>
2018-05-24 09:28:10 +0200

    BENCH: add benchmark of dense number nonconserving Hamiltonian

--- benchmarks/benchmark_dense.py
+++ benchmarks/benchmark_dense.py
@@ -5,7 +5,7 @@ import time
 import numpy as np
 
 sys.path.append('../')
-from pyexact.dense_operators import de_pc_op, de_sym_pc_op
+from pyexact.dense_operators import de_pc_op, de_sym_pc_op, de_npc_op
 
 
 def benchmark_dense_symmetric_number_conserving_op(L, N):
@@ -41,13 +41,42 @@ def benchmark_dense_number_conserving_op(L, N):
 
     return
 
+
+def benchmark_dense_number_nonconserving_op(L):
+    """Benchmark construction of number nonconserving operator."""
+    J = np.random.rand(L, L)
+    D = np.random.rand(L, L)
+    r = np.random.rand(L)
+    l = np.random.rand(L)
+    _ = de_npc_op(L, J, D, r, l)
+    t0 = time.time()
+    # Run once to compile with numba.
+    for _ in range(3):
+        de_pc_op(L, N, J, D)
+    t1 = time.time()
+    print('\nBenchmark: dense number nonconserving Hamiltonian.')
+    print(f'Time: {(t1-t0)/3:4.2f} s')
+
+    return
+
+
 if __name__ == '__main__':
 
+    # Number conserving.
     # System's length and particle number.
     L = 14
     N = L//2
 
-    print(f'Running benchmarks with {L} sites and {N} particles.')
+    print(f'Running number conserving benchmarks with {L} sites'
+          f' and {N} particles.')
 
     benchmark_dense_symmetric_number_conserving_op(L, N)
     benchmark_dense_number_conserving_op(L, N)
+
+    # Number nonconserving.
+    # System's length.
+    L = 14
+
+    print(f'Running number nonconserving benchmarks with {L} sites.')
+
+    benchmark_dense_number_nonconserving_op(L)
