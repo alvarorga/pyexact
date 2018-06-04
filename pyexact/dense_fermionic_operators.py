5b41cdab35035760e07bfd172e5a60789cf50a24
alvaro <alvarorga@hotmail.com>
2018-06-04 16:22:44 +0200

    ENH: add compute symmetric fermionic operators

--- pyexact/dense_fermionic_operators.py
+++ pyexact/dense_fermionic_operators.py
@@ -340,6 +340,7 @@ def de_npc_interaction(L, i, j):
 # Fermionic operators.
 
 
+@njit(parallel=True)
 def fer_de_pc_op(L, N, J, D):
     """Build a many-body fermionic operator with 2-particle terms.
 
@@ -383,6 +384,71 @@ def fer_de_pc_op(L, N, J, D):
                         H[ix_t, ix_s] += par*J[i, j]
 
                     # Interaction terms: n_i n_j.
+                    if (s>>i)&1 and (s>>j)&1:
+                        H[ix_s, ix_s] += D[i, j]
+
+    return H
+
+
+@njit(parallel=True)
+def fer_de_sym_pc_op(L, N, J, D):
+    """Build a many-body symmetric fermionic operator.
+
+    Note: numba parallel does not support the enumerate function, which
+    would make the code more readable. Instead we have to make the
+    outermost loops with range().
+
+    Args:
+        L (int): system's length.
+        N (int): particle number.
+        J (2darray of floats): symmetric hopping matrix.
+        D (2darray of floats): interaction matrix.
+
+    Returns:
+        H (2darray of floats): many-body operator.
+
+    """
+    if np.sum((J - J.T)**2) > 1e-7:
+        raise ValueError('J is not symmetric.')
+
+    # Put all elts of D in the lower triangle. Making a copy of D
+    # instead of working with views prevents the function to make
+    # changes in D outside of it.
+    D = np.copy(D)
+    for i in range(L):
+        for j in range(i):  # j < i.
+            D[i, j] += D[j, i]
+            D[j, i] = 0
+
+    states = generate_states(L, N)
+    num_states = states.size
+
+    H = np.zeros((num_states, num_states), np.float64)
+
+    # Notation:
+    #     s: initial state.
+    #     t: final state.
+    #     ix_#: index of #.
+    for ix_s in prange(num_states):
+        s = states[ix_s]
+        for i in range(L):
+            # On-site terms: n_i.
+            if np.abs(J[i, i]) > 1e-7:
+                if (s>>i)&1:
+                    H[ix_s, ix_s] += J[i, i]
+
+            for j in range(i):
+                # Hopping terms: b^dagger_i b_j.
+                if np.abs(J[i, j]) > 1e-7:
+                    if not (s>>i)&1 and (s>>j)&1:
+                        t = s + (1<<i) - (1<<j)
+                        par = get_parity(t, i, j)
+                        ix_t = np.where(states == t)[0][0]
+                        H[ix_t, ix_s] += par*J[i, j]
+                        H[ix_s, ix_t] += par*J[i, j]
+
+                # Interaction terms: n_i n_j.
+                if np.abs(D[i, j]) > 1e-7:
                     if (s>>i)&1 and (s>>j)&1:
                         H[ix_s, ix_s] += D[i, j]
 
--- tests/test_dense_fermionic_operators.py
+++ tests/test_dense_fermionic_operators.py
@@ -7,7 +7,7 @@ import numpy as np
 
 sys.path.append('../')
 from pyexact.dense_fermionic_operators import (
-    fer_de_pc_op
+    fer_de_pc_op, fer_de_sym_pc_op
     )
 
 class ManyBodyFermionicOperatorsTestCase(unittest.TestCase):
@@ -35,3 +35,31 @@ class ManyBodyFermionicOperatorsTestCase(unittest.TestCase):
         self.assertAlmostEqual(H[8, 2], 0)
         self.assertAlmostEqual(H[8, 3], -22)
         self.assertAlmostEqual(H[9, 2], 20)
+
+    def test_values_in_symmetric_operator_matrix(self):
+        """Test several values of a general symmetric operator."""
+        J = np.reshape(np.arange(25), (5, 5))
+        D = np.reshape(25+np.arange(25), (5, 5))
+        # Make J and D symmetric.
+        for j in range(5):
+            for i in range(j):
+                J[j, i] = J[i, j]
+                D[j, i] = D[i, j]
+        H = fer_de_sym_pc_op(5, 3, J, D)
+        self.assertAlmostEqual(H[0, 0], 188)
+        self.assertAlmostEqual(H[1, 1], 198)
+        self.assertAlmostEqual(H[3, 3], 242)
+        self.assertAlmostEqual(H[5, 5], 226)
+        self.assertAlmostEqual(H[9, 9], 296)
+        self.assertAlmostEqual(H[0, 1], 13)
+        self.assertAlmostEqual(H[0, 3], 3)
+        self.assertAlmostEqual(H[0, 5], -9)
+        self.assertAlmostEqual(H[0, 9], 0)
+        self.assertAlmostEqual(H[3, 5], 0)
+        self.assertAlmostEqual(H[4, 5], 7)
+        self.assertAlmostEqual(H[6, 9], -8)
+        self.assertAlmostEqual(H[7, 5], 13)
+        self.assertAlmostEqual(H[7, 6], 0)
+        self.assertAlmostEqual(H[8, 2], 0)
+        self.assertAlmostEqual(H[8, 3], -14)
+        self.assertAlmostEqual(H[9, 2], 4)
